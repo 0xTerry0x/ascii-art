9e9885173957708bdce10308fef51adcc9bf77b7
Kotsalas <christophorosk601@gmail.com>
2025-12-09 14:01:02 +0200

    clean folder set up

--- cmd/main.go
+++ cmd/main.go
@@ -22,15 +22,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	inputFile := "text/standard.txt"
+	inputFile := "../text/standard.txt"
 	if len(args) == 2 {
 		switch args[1] {
 		case "--standard":
-			inputFile = "text/standard.txt"
+			inputFile = "../text/standard.txt"
 		case "--shadow":
-			inputFile = "text/shadow.txt"
+			inputFile = "../text/shadow.txt"
 		case "--thinkertoy":
-			inputFile = "text/thinkertoy.txt"
+			inputFile = "../text/thinkertoy.txt"
 		default:
 			fmt.Println("Invalid style argument")
 			os.Exit(1)
