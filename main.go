0a830216ba0fbbdeb6c81cc1e14531748a5e6b29
lpapanthy <deezolut@gmail.com>
2025-12-09 12:54:28 +0200

    refactor: extract GenerateArt logic and separate entrypoint

--- main.go
+++ main.go
@@ -1,17 +1,80 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"os"
-
-	"asciiArt/internal"
 )
 
 func main() {
-	text, inputFile := internal.ParseArgs(os.Args)
+	args := os.Args[1:]
 
-	cleanText, indexes := internal.FilterPrintable(text)
+	if len(args) > 2 || len(args) < 1 {
+		fmt.Println("Incorrect amount of arguments")
+		os.Exit(1)
+	}
 
-	lines := internal.LoadFontFile(inputFile)
+	Text := args[0]
+	if len(Text) == 0 {
+		fmt.Println("Text argument is empty")
+		os.Exit(1)
+	}
 
-	internal.PrintAscii(cleanText, lines, indexes)
+	inputFile := "text/standard.txt"
+	if len(args) == 2 {
+		switch args[1] {
+		case "--standard":
+			inputFile = "text/standard.txt"
+		case "--shadow":
+			inputFile = "text/shadow.txt"
+		case "--thinkertoy":
+			inputFile = "text/thinkertoy.txt"
+		default:
+			fmt.Println("Invalid style argument")
+			os.Exit(1)
+		}
+	}
+
+	newText := ""
+	var indexes []int
+
+	runeIndex := 0
+	for _, ch := range Text {
+		if ch < 32 || ch > 126 {
+			indexes = append(indexes, runeIndex)
+		} else {
+			newText += string(ch)
+		}
+		runeIndex++
+	}
+
+	file, err := os.Open(inputFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	output := GenerateArt(newText, lines)
+	fmt.Print(output)
+
+	if len(indexes) > 0 {
+
+		fmt.Print("Non-Printable characters: \n")
+		for i, v := range indexes {
+			fmt.Print(v)
+
+			if i != len(indexes)-1 {
+				fmt.Print(", ")
+			} else {
+				fmt.Println()
+			}
+
+		}
+	}
 }
